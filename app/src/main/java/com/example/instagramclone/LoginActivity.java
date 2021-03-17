151b1eb096c4f8923ae1b25d046550513e3fe816
Ivan Yeung <42223424+RamenWarlord@users.noreply.github.com>
2021-03-17 01:26:09 -0400

    added signup function

--- app/src/main/java/com/example/instagramclone/LoginActivity.java
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -8,10 +8,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,7 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
-
+    private Button btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +44,55 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        btnSignup = findViewById(R.id.btnSignup);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick login button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                if(username.isEmpty())
+                {
+                    Toast.makeText(LoginActivity.this, "Username missing for signup", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(password.isEmpty())
+                {
+                    Toast.makeText(LoginActivity.this, "Password missing for signup", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                signUpUser(username, password);
+            }
+        });
+
+    }
+
+    private void signUpUser(String username, String password)
+    {
+        // Create the ParseUser
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        //user.setEmail("email@example.com");
+        // Set custom properties
+        //user.put("phone", "650-253-0000");
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                    Toast.makeText(LoginActivity.this, "Signup successful, please re-enter to login", Toast.LENGTH_SHORT).show();
+                    etUsername.setText("");
+                    etPassword.setText("");
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                    Toast.makeText(LoginActivity.this, "Username taken, use another", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "Error signing up " + e);
+                }
+            }
+        });
     }
 
     private void loginUser(String username, String password)
