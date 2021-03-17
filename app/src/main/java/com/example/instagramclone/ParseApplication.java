f55028a3eacbd9c684ecf4f1bfa2d902d34c644a
Ivan Yeung <42223424+RamenWarlord@users.noreply.github.com>
2021-03-16 23:53:26 -0400

    Initial commit

---
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,23 @@
+package com.example.instagramclone;
+
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+        // ParseObject.registerSubclass(Post.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
